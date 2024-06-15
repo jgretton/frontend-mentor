fc7a91347031b05460011b08edcab7afaaf1ebaa
Joshua Gretton <jb.gretton@googlemail.com>
2024-06-15 15:11:28 +0100

    Update tailwind.config.js
    Added styles for challenge 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				//Challenge 1
+				Nutmeg: "hsl(14, 45%, 36%)",
+				"Dark-Raspberry": "hsl(332, 51%, 32%)",
+				"Rose-White": "hsl(330, 100%, 98%)",
+				Eggshell: "hsl(30, 54%, 90%)",
+				"Light-Grey": "hsl(30, 18%, 87%)",
+				"Wenge-Brown": "hsl(30, 10%, 34%)",
+				"Dark-Charcoal": "hsl(24, 5%, 18%)",
+			},
+		},
 	},
 	plugins: [],
 };
