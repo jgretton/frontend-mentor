3fc98eb7436254061e0257be7acc73f22a9fc282
Joshua Gretton <jb.gretton@googlemail.com>
2024-06-15 14:36:30 +0100

    Update page.js
    Navigation for challenges added to homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
+const links = [{ name: "Challenge 1 - Receipe page", href: "/challenges/1" }];
+
 export default function Home() {
-	return <main className=""></main>;
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<ul>
+				{links.map((link, index) => (
+					<li key={index}>
+						<Link className="" href={link.href}>
+							{link.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
 }
