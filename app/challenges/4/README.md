5c708324a772862edff3c6cde317624b93f5368f
Joshua Gretton <jb.gretton@googlemail.com>
2024-06-21 17:15:08 +0100

    Completed challenge 4
    Challenge 4 code along with completed screenshot and updated README

---
+++ app/challenges/4/README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Social links profile solution
+
+This is a solution to the [Social links profile challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Social links profile solution](#frontend-mentor---social-links-profile-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+    - [Built with](#built-with)
+    - [Continued development](#continued-development)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+
+## Overview
+
+Your challenge is to build out this results summary component and get it looking as close to the design as possible. The design used was a JPEG file so i had to eye-ball the margins, text-size and padding.
+This is the challenge outline :
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: Use the local JSON data to dynamically populate the content
+
+### Screenshot
+
+![Completed screenshot of recipe page](./completed-screenshot.png)
+
+### Links
+
+To be added
+
+### Built with
+
+- Semantic HTML5 markup
+- Tailwind CSS
+- Mobile-first workflow
+- [Next.js](https://nextjs.org/)
+
+### Continued development
+
+Moving forwards to the next challange, I plan on using what I have learned about semantic markup and accessible code with screenreaders, and implementing it to the best of my ability.
+
+### Useful resources
+
+## Author
+
+- Website - [Josh Gretton](https://www.joshgretton.co.uk)
+
+```
+
+```
--- app/challenges/4/page.js
+++ app/challenges/4/page.js
@@ -1,25 +1,36 @@
 import Link from "next/link";
 import React from "react";
+
+import { Verbal, Reaction, Memory, Visual } from "./svgs";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const Results = [
   {
     category: "Reaction",
     score: 80,
-    icon: "./icon-reaction.svg",
+    icon: Reaction,
+    colour: "red",
   },
   {
     category: "Memory",
     score: 92,
-    icon: "./icon-memory.svg",
+    icon: Memory,
+    colour: "yellow",
   },
   {
     category: "Verbal",
     score: 61,
-    icon: "./icon-verbal.svg",
+    icon: Verbal,
+    colour: "green",
   },
   {
     category: "Visual",
     score: 72,
-    icon: "./icon-visual.svg",
+    icon: Visual,
+    colour: "purple",
   },
 ];
 
@@ -27,15 +38,15 @@ const averageScore = (results) => {};
 
 const page = () => {
   return (
-    <main>
-      <article className="">
-        <section className="flex flex-col items-center justify-center gap-7 rounded-b-[2.5rem] bg-gradient-to-b from-[hsl(252,100%,67%)] to-[hsl(241,81%,54%)] pb-10 pt-7 text-white">
-          <h2 className="text-2xl font-medium text-white/70">Your Result</h2>
-          <div className="flex size-36 flex-col items-center justify-center rounded-full bg-gradient-to-b from-[hsl(241,81%,54%)]">
-            <p className="text-5xl font-bold">76</p>
+    <main className="min-h-screen sm:grid sm:place-items-center">
+      <article className="mx-auto flex w-full max-w-3xl flex-col sm:flex-row sm:rounded-[2.5rem] sm:shadow-2xl">
+        <section className="flex w-full flex-col items-center justify-between gap-7 rounded-b-[2.5rem] bg-gradient-to-b from-[hsl(252,100%,67%)] to-[hsl(241,81%,54%)] pb-10 pt-7 text-white sm:rounded-[2.5rem] sm:pt-10">
+          <h1 className="text-2xl font-medium text-white/70">Your Result</h1>
+          <div className="flex size-40 flex-col items-center justify-center gap-2 rounded-full bg-gradient-to-b from-[hsl(241,81%,54%)] sm:size-52">
+            <p className="text-5xl font-bold sm:text-6xl">76</p>
             <p className="text-white/50">of 100</p>
           </div>
-          <div className="max-w-xs space-y-3 text-center">
+          <div className="max-w-xs space-y-3 px-4 text-center">
             <p className="text-3xl font-semibold">Great </p>
             <p className="text-lg text-white/70">
               You scored higher than 65% of the people who have taken these
@@ -43,25 +54,31 @@ const page = () => {
             </p>
           </div>
         </section>
-        <section className="mt-8 px-7">
+        <section className="mt-8 w-full px-7 pb-10 pt-7 sm:m-0 sm:pt-10">
           <h2 className="text-xl font-semibold">Summary</h2>
           <ul className="mt-8 grid gap-4">
             {Results.map((result, index) => {
-              const { icon: Icon } = result;
+              const { icon: Icon, colour } = result;
+              const colours = {
+                red: "text-red-400 bg-red-400/10",
+                yellow: "text-yellow-400 bg-yellow-400/10",
+                green: "text-green-400 bg-green-400/10",
+                purple: "text-blue-600 bg-blue-400/10",
+              };
               return (
                 <li
                   key={index}
-                  className="flex justify-between rounded-lg bg-red-100/70 p-4"
+                  className={`flex justify-between rounded-lg p-4 ${colours[colour]}`}
                 >
                   <div className="">
-                    <p className="text-lg font-semibold text-red-600/70">
-                      <Icon className="size-4" />
+                    <p className="text-g-600/70 inline-flex items-center gap-3 text-lg font-semibold">
+                      <Icon className="size-5" />
                       {result.category}
                     </p>
                   </div>
                   <div className="">
-                    <p className="font-semibold">
-                      {result.score}{" "}
+                    <p className="font-semibold text-black">
+                      {result.score}
                       <span className="text-black/60"> / 100</span>
                     </p>
                   </div>
@@ -69,7 +86,11 @@ const page = () => {
               );
             })}
           </ul>
-          <Link href={"#"} role="button">
+          <Link
+            href={"#"}
+            role="button"
+            className="mt-6 block rounded-full bg-slate-700 from-[hsl(252,100%,67%)] to-[hsl(241,81%,54%)] py-4 text-center text-xl font-semibold text-white transition-colors hover:bg-gradient-to-b"
+          >
             Continue
           </Link>
         </section>
